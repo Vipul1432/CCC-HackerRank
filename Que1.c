aaddcff2bca21e8605e3f1b04a5d22064dff4d2e
Mr.Vipul <vipulupadhyay563@gmail.com>
2022-10-03 14:41:26 +0530

    Add  Two Number

---
+++ Que1.c
@@ -0,0 +1,50 @@
+/* Given two values, they can be integer or floating point numbers, add them.
+
+Input Format
+
+Two values separated by a space.
+
+Constraints
+
+No constraints.
+
+Output Format
+
+One value, the result of sum of two input values.
+
+Sample Input 0
+
+5 4
+Sample Output 0
+
+9
+Explanation 0
+
+5 + 4 = 9 */
+
+//  Solution
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    float a, b;
+    scanf("%f%f", &a, &b);
+
+    long int c = a + b;
+
+    if (c < (a + b))
+
+        printf("%.2f", a + b);
+
+    else
+
+        printf("%ld", c);
+
+    return 0;
+}
