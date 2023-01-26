efafaa5232977a2d5985253190e28165a699f70f
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-01-26 13:53:45 +0530

    Caesar Cipher
    Caesar Cipher

---
+++ Que24.c
@@ -0,0 +1,99 @@
+/*
+
+Caesar Cipher
+
+Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+Example
+
+
+The alphabet is rotated by , matching the mapping above. The encrypted string is .
+
+Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
+
+Function Description
+
+Complete the caesarCipher function in the editor below.
+
+caesarCipher has the following parameter(s):
+
+string s: cleartext
+int k: the alphabet rotation factor
+Returns
+
+string: the encrypted string
+Input Format
+
+The first line contains the integer, , the length of the unencrypted string.
+The second line contains the unencrypted string, .
+The third line contains , the number of letters to rotate the alphabet by.
+
+Constraints
+
+
+
+ is a valid ASCII string without any spaces.
+
+Sample Input
+
+11
+middle-Outz
+2
+Sample Output
+
+okffng-Qwvb
+Explanation
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +2:    cdefghijklmnopqrstuvwxyzab
+
+m -> o
+i -> k
+d -> f
+d -> f
+l -> n
+e -> g
+-    -
+O -> Q
+u -> w
+t -> v
+z -> b
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int n;
+    char s[101];
+    int k;
+    scanf("%d %s %d", &n, s, &k);
+    k %= 26;
+    for (i = 0; i < n; i++)
+    {
+        if ((s[i] >= 'A') && (s[i] <= 'Z'))
+        {
+            if ((s[i] + k >= 'A') && (s[i] + k <= 'Z'))
+                printf("%c", s[i] + k);
+            else
+                printf("%c", 'A' + (((s[i]) + k) - 'Z') - 1);
+        }
+        else
+        {
+            if ((s[i] >= 'a') && (s[i] <= 'z'))
+                if (((s[i]) + k >= 'a') && ((s[i]) + k <= 'z'))
+                    printf("%c", (s[i]) + k);
+                else
+                    printf("%c", 'a' + (((s[i]) + k) - 'z') - 1);
+            else
+                printf("%c", s[i]);
+        }
+    }
+    return 0;
+}
